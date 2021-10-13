342bc5ac530a89f5094dfdc4175c25e3c41b5ce7
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-10-13 20:52:56 +0545

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,15 @@
+$ nano name.sh
+#! /bin/bash
+echo "Hello World"
+ctrl X to save
+$ ls
+$ chmod +x name.sh
+$ bash name.sh
+$ ./name.sh
+# 2 file 
+$ cd /storage/emulated/0/
+$ ls
+$ bash name.sh
+# easy to run code tetmux shell
+$ chmod +x install.sh
+$ bash name.sh
